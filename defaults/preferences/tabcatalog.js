1d7602ec387cf41700ece9cd1d488b4d30fbfd33
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2009-11-04 06:45:14 +0000

    デフォルトで背景を描画するようにした
    git-svn-id: http://www.cozmixng.org/repos/piro/tabcatalog/trunk@5405 599a83e7-65a4-db11-8015-0010dcdd6dc2

--- defaults/preferences/tabcatalog.js
+++ defaults/preferences/tabcatalog.js
@@ -30,7 +30,7 @@ pref("extensions.tabcatalog.override.allinonegest", true);
 
 pref("extensions.tabcatalog.zoom.reverseWheelScrollDirection", false);
 
-pref("extensions.tabcatalog.rendering_quality", 1);
+pref("extensions.tabcatalog.rendering_quality", 2);
 
 pref("extensions.tabcatalog.animation.scroll.enabled", true);
 pref("extensions.tabcatalog.animation.scroll.timeout", 150);
